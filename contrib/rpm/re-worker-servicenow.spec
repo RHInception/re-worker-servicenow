cbd5c26bbf3a976ead117ef2d392d6b1a11b4162
Steve Milner <stevem@gnulinux.net>
2014-07-17 11:54:07 -0400

    Initial import.

---
+++ contrib/rpm/re-worker-servicenow.spec
@@ -0,0 +1,46 @@
+%if 0%{?rhel} && 0%{?rhel} <= 6
+%{!?__python2: %global __python2 /usr/bin/python2}
+%{!?python2_sitelib: %global python2_sitelib %(%{__python2} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")}
+%{!?python2_sitearch: %global python2_sitearch %(%{__python2} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib(1))")}
+%endif
+
+%global _pkg_name replugin
+%global _src_name reworkerservicenow
+
+Name: re-worker-servicenow
+Summary: ServiceNow worker for Release Engine
+Version: 0.0.1
+Release: 1%{?dist}
+
+Group: Applications/System
+License: AGPLv3
+Source0: %{_src_name}-%{version}.tar.gz
+Url: https://github.com/rhinception/re-worker-servicenow
+
+BuildArch: noarch
+BuildRequires: python2-devel, python-setuptools
+Requires: re-worker, python-requests
+
+%description
+A ServiceNow worker for Rease Engine provides basic change record
+access.
+
+%prep
+%setup -q -n %{_src_name}-%{version}
+
+%build
+%{__python2} setup.py build
+
+%install
+%{__python2} setup.py install -O1 --root=$RPM_BUILD_ROOT --record=re-worker-servicenow-files.txt
+
+%files -f re-worker-servicenow-files.txt
+%defattr(-, root, root)
+%doc README.md LICENSE AUTHORS
+%dir %{python2_sitelib}/%{_pkg_name}
+%exclude %{python2_sitelib}/%{_pkg_name}/__init__.py*
+
+
+%changelog
+* Thu Jul 17 2014 Steve Milner <stevem@gnulinux.net> - 0.0.1-1
+- Initial spec
---
+++ test/test_servicenow_worker.py
@@ -0,0 +1,109 @@
+# Copyright (C) 2014 SEE AUTHORS FILE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Unittests.
+"""
+
+import pika
+import mock
+
+from contextlib import nested
+
+from . import TestCase
+
+from replugin import servicenowworker
+
+
+MQ_CONF = {
+    'server': '127.0.0.1',
+    'port': 5672,
+    'vhost': '/',
+    'user': 'guest',
+    'password': 'guest',
+}
+
+
+class TestServiceNowWorker(TestCase):
+
+    def setUp(self):
+        """
+        Set up some reusable mocks.
+        """
+        TestCase.setUp(self)
+
+        self.channel = mock.MagicMock('pika.spec.Channel')
+
+        self.channel.basic_consume = mock.Mock('basic_consume')
+        self.channel.basic_ack = mock.Mock('basic_ack')
+        self.channel.basic_publish = mock.Mock('basic_publish')
+
+        self.basic_deliver = mock.MagicMock()
+        self.basic_deliver.delivery_tag = 123
+
+        self.properties = mock.MagicMock(
+            'pika.spec.BasicProperties',
+            correlation_id=123,
+            reply_to='me')
+
+        self.logger = mock.MagicMock('logging.Logger').__call__()
+        self.app_logger = mock.MagicMock('logging.Logger').__call__()
+        self.connection = mock.MagicMock('pika.SelectConnection')
+
+    def tearDown(self):
+        """
+        After every test.
+        """
+        TestCase.tearDown(self)
+        self.channel.reset_mock()
+        self.channel.basic_consume.reset_mock()
+        self.channel.basic_ack.reset_mock()
+        self.channel.basic_publish.reset_mock()
+
+        self.basic_deliver.reset_mock()
+        self.properties.reset_mock()
+
+        self.logger.reset_mock()
+        self.app_logger.reset_mock()
+        self.connection.reset_mock()
+
+    def test_does_change_record_exist(self):
+        """
+        Verifies checking for change records results in the proper responses.
+        """
+        with nested(
+                mock.patch('pika.SelectConnection'),
+                mock.patch('replugin.servicenowworker.ServiceNowWorker.notify'),
+                mock.patch('replugin.servicenowworker.ServiceNowWorker.send')):
+
+            worker = servicenowworker.ServiceNowWorker(
+                MQ_CONF,
+                logger=self.app_logger,
+                config_file='conf/example.json')
+
+            worker._on_open(self.connection)
+            worker._on_channel_open(self.channel)
+
+            body = {
+            }
+
+            # Execute the call
+            worker.process(
+                self.channel,
+                self.basic_deliver,
+                self.properties,
+                body,
+                self.logger)
+
+            assert self.app_logger.error.call_count == 0
